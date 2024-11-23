512541d006266ae993c8fbe1d0d55df024bdeb22
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-11-23 19:23:11 +0530

    Add files via upload

---
+++ Main (13).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+  
+ int x = 8, y = 57;
+
+  cout << "x == y: " << (x == y) << endl;
+ cout << "x != y: " <<(x != y) << endl;
+ cout << "x < y: " << (x < y) << endl;
+ cout << "x > y: " <<(x > y) << endl;
+ cout << "x <= y: " << (x <= y) << endl;
+ cout << "x >= y: " << (x >= y) << endl;
+
+return 0;
+
+}
+
+
