a5ff652d0960a573934944224e871ea73cd894c1
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-11-23 19:25:51 +0530

    Add files via upload

---
+++ Do While Loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using  namespace  std ; 
+int  main () { 
+int  i =  1 ;  // Initialization 
+do  { 
+cout  << i <<  " " ; 
+i++;  // Update 
+}  while  (i <=  5 );  // Condition 
+return  0 ; 
+} 
