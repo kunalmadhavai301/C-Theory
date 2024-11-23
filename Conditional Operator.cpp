29241079bac7489cb0c1068661e7f0943fd69753
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-11-23 19:25:15 +0530

    Add files via upload

---
+++ Conditional Operator.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+using  namespace  std ; 
+int  main () { 
+int  a =  15 , b =  25 ; 
+int  max = (a > b) ? a : b; 
+cout  <<  "The maximum is: "  << max <<  endl ; 
+return  0 ; 
+}
