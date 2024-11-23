92073fee7ca833ac3faf233b3c0070675fd6a8b6
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-11-23 19:24:56 +0530

    Add files via upload

---
+++ Checking Voting Eligibility.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using  namespace  std ; 
+int  main () { 
+int  age; 
+cout  <<  "Enter your age: " ; 
+cin  >> age; 
+string  eligibility = (age >=  18 ) ?  "Eligible to  vote"  :  "Not eligible to vote" ; 
+cout  << eligibility <<  endl ; 
+return  0 ; 
+} 
