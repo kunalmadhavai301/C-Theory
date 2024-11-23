d137c2e7ae1eb85296c9a0cbd14856f46158d458
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-11-23 19:22:06 +0530

    Add files via upload

---
+++ Info.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+ int age = 45; // Integer
+ 
+ float height = 4.35; // Float
+ 
+ char grade = 'v'; // Character
+ 
+ bool isStudent = false; // Boolean
+ 
+ double pi = 3.141592659; // Double
+ 
+ cout << "Age: " << age << endl;
+ cout << "Height: " << height << " feet" << endl;
+ cout << "Grade: " << grade << endl;
+ cout << "Is Student: " << isStudent << endl;
+ cout << "Value of Pi: " << pi << endl;
+ 
+ return 0;
+}
