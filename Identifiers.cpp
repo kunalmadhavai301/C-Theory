3f7980eab825ff5582448129070eafe07d3b4e8d
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-11-23 19:28:29 +0530

    Add files via upload

---
+++ Identifiers.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using  namespace  std ; 
+int  main () { 
+int  age =  20 ;  
+float  height =  5.9 ;  
+// 'age' is an identifier 
+// 'height' is an identifier 
+cout  <<  "Age: "  << age <<  endl ; 
+cout  <<  "Height: "  << height <<  endl ; 
+return  0 ;  
+// 'main' and 'return' are keywords, not identifiers
+}
