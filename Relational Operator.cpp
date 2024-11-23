98a107007090bb65fefcb4eb8eff53e216cece20
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-11-23 19:32:07 +0530

    Add files via upload

---
+++ Relational Operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using  namespace  std ; 
+
+int  main () { 
+  int  x =  10 , y =  19  ; 
+  cout  <<  "x == y: "  << (x == y) <<  endl ; 
+  cout  <<  "x != y: "  << (x != y) <<  endl ; 
+  cout  <<  "x < y: "  << (x < y) <<  endl ; 
+  cout  <<  "x > y: "  << (x > y) <<  endl ; 
+  cout  <<  "x <= y: "  << (x <= y) <<  endl ; 
+  cout  <<  "x >= y: "  << (x >= y) <<  endl ; 
+  
+  return  0 ; 
+} 
