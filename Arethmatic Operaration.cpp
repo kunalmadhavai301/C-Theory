cc075465b2bd0169b31b1236545a752572d8454b
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-11-23 19:21:30 +0530

    Add files via upload

---
+++ Arethmatic Operaration.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+   
+ int a = 17, b = 2;
+ 
+ cout << "Addition: " << a + b << endl;
+ cout << "Subtraction: " << a - b << endl;
+ cout << "Multiplication: " << a * b << endl;
+ cout << "Division: " <<a / b << endl;
+ cout << "Remainder: " << a % b << endl;
+ 
+ return 0;
+ 
+}
+
