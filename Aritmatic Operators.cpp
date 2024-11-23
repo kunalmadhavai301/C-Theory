8f94f7b5395be7d14c6def9f25af77cba2f11132
KUNAL MADHAVAI <kunal.madhavai24@sanjivani.edu.in>
2024-11-23 19:24:11 +0530

    Add files via upload

---
+++ Aritmatic Operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using  namespace  std ; 
+
+int  main () { 
+  int  a =  40, b =  50 ; 
+  cout  <<  "Addition: "  << a + b <<  endl ; 
+  cout  <<  "Subtraction: "  << a - b <<  endl ; 
+  cout  <<  "Multiplication: "  << a * b <<  endl ; 
+  cout  <<  "Division: "  << a / b <<  endl ; 
+  cout  <<  "Remainder: "  << a % b <<  endl ; 
+  
+  return  0 ; 
+} 
